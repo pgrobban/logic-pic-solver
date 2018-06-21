6209a785d10dadd9b04907437b290499bceec66d
pgrobban <robert.sebescen@surikat.se>
2018-06-21 10:44:19 +0200

    added test case for level 13

--- test/index-test.js
+++ test/index-test.js
@@ -538,4 +538,16 @@ describe('Solver', () => {
     const actualSolution = solve(LEVELS.FIRST_STEPS[11]);
     should.deepEqual(expectedSolution, actualSolution);
   });
+
+  it('Should solve First steps 13', () => {
+    const expectedSolution = [
+      ['X', 'O', 'O', 'X', 'X'],
+      ['O', 'O', 'O', 'X', 'X'],
+      ['X', 'O', 'O', 'O', 'X'],
+      ['X', 'O', 'O', 'O', 'O'],
+      ['X', 'X', 'O', 'X', 'X']
+    ];
+    const actualSolution = solve(LEVELS.FIRST_STEPS[12]);
+    should.deepEqual(expectedSolution, actualSolution);
+  });
 })
